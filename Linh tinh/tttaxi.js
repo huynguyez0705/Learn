ab4e4ac67f91e4a4766701b095a7d57ad0d0a937
unknown <huy751998@gmail.com>
2024-02-26 16:30:01 +0700

    new

---
+++ Linh tinh/tttaxi.js
@@ -0,0 +1,37 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// ;Số km ≤ 1 giá 15000đ
+// 1 < số km ≤ 5 giá 13500đ
+// Số km > 5 giá 11000đ
+// Nếu số km > 120 km sẽ được giảm 10% trên tổng số tiền.
+
+function tinhtienTaxi(soKm) {
+  let gia = 0;
+  if (soKm <= 1) {
+    gia = 15000;
+  } else if (soKm <= 5) {
+    gia = 13500;
+  } else {
+    gia = 11000;
+  }
+
+  let tongTien = soKm * gia;
+  if (soKm > 120) {
+    tongTien = tongTien * 0.9;
+  }
+  return tongTien;
+}
+function main() {
+  rl.question(
+    "Nhập vào số kilometer đã đi hoặc 'exit' để kết thúc: ",
+    (soKm) => {
+      const giaTien = tinhtienTaxi(Number(soKm));
+      console.log(`Bạn phải trả ${giaTien} đ cho ${soKm} km `);
+      main();
+    }
+  );
+}
+main();
--- test.js
+++
@@ -1,4 +0,0 @@
-const arr1 = [1, 2, 3, 4];
-const arr2 = [7, 8, 9, 10];
-const arr3 = arr1.concat(...arr2);
-console.log(arr3);
